d537be4de3965fb563bd7d767798706effabd0e8
annivs <anni.saarelma@icloud.com>
2024-01-17 12:57:26 +0200

    Tehtävä 3

---
+++ moduuli 1/tehtävä 3.py
@@ -0,0 +1,10 @@
+
+kanta = int(input("Mikä on suorakulmion kanta?"))
+korkeus = int(input("Mikä on suorakulmion korkeus?"))
+pinta = str(kanta * korkeus)
+piiri = str(kanta * 2 + korkeus * 2)
+print ("Suorakulmion pinta-ala " + pinta + " ja piiri on " + piiri)
+
+
+
+
