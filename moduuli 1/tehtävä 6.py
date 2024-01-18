8cf7adabbefba68e5285adc9bfefb2a28a984793
annivs <anni.saarelma@icloud.com>
2024-01-18 18:45:15 +0200

    Tehtävä 6

---
+++ moduuli 1/tehtävä 6.py
@@ -0,0 +1,6 @@
+import random
+random_kolmiluku = random.randint(100, 999)
+print("Kolmiluku: " + str(random_kolmiluku) + ".")
+random_neliluku = random.randint(1000, 9999)
+print("Neliluku: " + str(random_neliluku) + ".")
+
