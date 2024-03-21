ac0aae50e1381a80dad0f8c32f6f1af30789429a
annivs <anni.saarelma@icloud.com>
2024-03-21 20:56:56 +0200

    moi lol

---
+++ moduuli 9/teht. 2.py
@@ -0,0 +1,40 @@
+class Auto:
+
+    auto_count = 0
+
+    def __init__(self, rekkari, huippunopeus):
+        Auto.auto_count = Auto.auto_count + 1
+        print(f"Uusi auto, autoja on nyt {Auto.auto_count} kpl")
+        self.rekkari = rekkari
+        self.nopeus = 0
+        self.kuljettu_matka = 0
+        self.huippunopeus = huippunopeus
+
+
+    def print_info(self):
+        print(f"{self.rekkari} {self.huippunopeus}")
+        print(f"{self.nopeus} {self.kuljettu_matka}")
+
+    def kiihdytä(self, nopeuden_muutos):
+        uusi_nopeus = self.nopeus + nopeuden_muutos
+        if uusi_nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+        elif uusi_nopeus < 0:
+            self.nopeus = 0
+        else:
+            self.nopeus = uusi_nopeus
+
+
+auto = Auto('ABC-123',1000)
+    #kiihdytys
+auto.kiihdytä(30)
+auto.kiihdytä(70)
+auto.kiihdytä(50)
+print(f"Auton uusi nopeus:", auto.nopeus, "km/h")
+
+    #hätäjarrutus
+auto.kiihdytä(-200)
+print("Auton nopeus hätäjarrutuksen jälkeen:", auto.nopeus, "km/h")
+
+
+
